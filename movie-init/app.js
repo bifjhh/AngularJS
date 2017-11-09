e425ce2521d2fbf48e6e89c1246d095df33bfba5
bifjhh <929160004@qq.com>
2017-11-09 14:56:13 +0800

    检测锚点值的变化改变导航栏的焦点状态

--- movie-init/app.js
+++ movie-init/app.js
@@ -1,7 +1,27 @@
 (function (angular) {
     // "use strict";
     var myApp = angular.module('myApp', ['ngRoute', 'myApp1', 'myApp2', 'myApp3', 'myApp4','myjsonp'])
-        ; 
+        ; myApp.directive('autoActive', ['$location', function ($location){
+            return{
+                link:function(scope,element,attributes){
+                    // console.log(1);
+                  /*   element.on('click',function(){
+                        element.parent().children().removeClass('active')
+                        ;element.addClass('active')
+                    }) */
+                    scope.loca = $location
+                    ;scope.$watch('loca.url()',function(){
+                        // console.log(123);
+                        var hash =$location.url()
+                        ;var val = element.find('a').attr('href').substr(1)
+                        ;if(hash==val){
+                            element.parent().children().removeClass('active')
+                            ; element.addClass('active')
+                        }
+                    })
+                }
+            }
+        }])
 })(angular);
 
 /* 
